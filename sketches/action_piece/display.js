980b761d02a68cb4fa01a57c681dbdf0d7ad297c
action-script <mail.actionscript@gmail.com>
2018-07-03 19:53:17 +0200

    OTSParty2018 | action-piece - proof of concept

--- sketches/action_piece/display.js
+++ sketches/action_piece/display.js
@@ -10,6 +10,41 @@ socket.on('reload', function(piecename) {
   location.reload();
 });
 
+var phones = {};
+var phonePoints = []; 
+
+// get phones
+socket.on('dataChannel3', function(data) {
+   if (data.topic == 'request') {
+      phones[data.id] = {id: data.id}
+      if (phonePoints.length <= 8) {
+         phonePoints.push( phones[data.id].id );
+         socket.emit('dataChannel3', {
+            'topic': 'accepted',
+            'to': phones[data.id].id,
+         });
+      } else
+         socket.emit('dataChannel3', {
+            'topic': 'busy',
+            'to': phones[data.id].id,
+         });
+   }
+});
+
+socket.on('dataChannel1', function(data) {
+   if (data.topic == 'point' && phones[data.id] != undefined) {
+      phones[data.id].point = data.point
+      //CABLES.patch.setVariable("pointIndex", 0);
+      //CABLES.patch.setVariable("pointPosition", [data.point.x, data.point.y, Math.sin(new Date().getTime()/1000.0)]);
+      //CABLES.patch.config.setPoint();
+   }
+});
+
+
+// TODO: save all connection in array,
+// Use only 8 first ones and queue the rest.
+// on dissconnect, rearange
+
 function showError(errId, errMsg) {
    alert('An error occured: ' + errId + ', ' + errMsg);
 }
@@ -17,23 +52,26 @@ function showError(errId, errMsg) {
 function patchInitialized() {
    // You can now access the patch object (CABLES.patch), register variable watchers and so on
    setInterval(function() {
-      for (var i = 0; i < 8; i++) {
-         var arr = [0, 0, 0];
-         arr[0] = Math.random();
-         arr[1] = Math.random();
-         arr[2] = Math.random();
-         CABLES.patch.setVariable("pointIndex",i);
-         CABLES.patch.setVariable("pointPosition", arr);
-         CABLES.patch.config.setPoint();
+      for (var i in phonePoints) {
+         var id = phonePoints[i];
+         var phone = phones[id];
+         if (phone.point != undefined) {
+            var point = [phone.point.x, phone.point.y];
+            point[2] = Math.sin(new Date().getTime()/1000.0);
+            CABLES.patch.setVariable("pointIndex", i);
+            CABLES.patch.setVariable("pointPosition", point);
+            CABLES.patch.config.setPoint();
+         }
       }
-   }, 2);
+   }, 1);
 }
 
 function patchFinishedLoading() {
    // The patch is ready now, all assets have been loaded
 }
 
-document.addEventListener('DOMContentLoaded', function(event) {
+
+//document.addEventListener('DOMContentLoaded', function(event) {
    CABLES.patch = new CABLES.Patch({
        patchFile: 'js/OST_test01.json',
        prefixAssetPath: '',
@@ -43,6 +81,6 @@ document.addEventListener('DOMContentLoaded', function(event) {
        onPatchLoaded: patchInitialized,
        onFinishedLoading: patchFinishedLoading,
    });
-});
+//});
 
 
