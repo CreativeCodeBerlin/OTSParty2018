853e978d46fd6c067fdc06ab02253263965dc271
Abe Pazos <abe@hamoid.com>
2018-07-06 00:39:23 +0200

    abe: add particle system

--- sketches/abe/display.js
+++ sketches/abe/display.js
@@ -1,10 +1,13 @@
 var phones = {};
+var pg;
 var fx;
+var tex;
+var texNum = Math.floor(Math.random() * 40);
+var particles = [];
 
 var CMD_START = 0;
 var CMD_MOVE = 1;
 var CMD_END = 2;
-var CMD_COLOR = 3;
 
 // --- socket ---
 var socket = io();
@@ -28,12 +31,21 @@ socket.on('dataChannel1', function(data) {
   }
   var phone = phones[data.id];
 
+  if(data.cmd == CMD_START) {
+    phone.color = tex.get(
+      data.x * tex.width,
+      data.y * tex.height);
+  }
   if(data.cmd <= CMD_END) {
     phone.last = new Date().getTime();
     phone.x = data.x;
     phone.y = data.y;
-  } else if(data.cmd == CMD_COLOR) {
-    phone.color = color(random(255), random(255), random(255));
+    particles.push({
+      x: phone.x,
+      y: phone.y,
+      color: phone.color,
+      age: 1.0
+    });
   }
 });
 
@@ -42,55 +54,70 @@ socket.on('dataChannel1', function(data) {
 function preload() {
   var r = Math.random();
   fx = loadShader('vert.glsl?r='+r, 'frag.glsl?r='+r);
+  tex = loadImage('media/' + nf(texNum, 2) + '.jpg');
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
-  setAttributes('antialias', true);
-  shader(fx);
+  pg = createGraphics(windowWidth, windowHeight, WEBGL);
+  //pg.setAttributes('antialias', true);
+  pg.shader(fx);
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+  var newPG = createGraphics(windowWidth, windowHeight);
+  newPG.image(pg, 0, 0, newPG.width, newPG.height);
+  pg = newPG;
 }
 
 function draw() {
   var t = millis() * 0.001;
   fx.setUniform('t', t);
 
-  //background(0);
+  if(frameCount % (60 * 15) == 0) {
+    texNum = floor(random(40));
+    tex = loadImage('media/' + nf(texNum, 2) + '.jpg');
+  }
+
   drawCubes();
 }
 
 // draw a cube at the position of each phone input
 function drawCubes() {
-  push();
-  translate(-width/2, -height/2);
+  pg.clear();
+  pg.push();
+  pg.translate(-width/2, -height/2);
+  pg.directionalLight(100, 100, 100, -1, -1, -1);
+  pg.noStroke();
 
-  for(var id in phones) {
-    var phone = phones[id];
-    if (phone.x) {
-      push();
+  for(var id in particles) {
 
-      // fade cube out when is inactive
-      var timeDistance = new Date().getTime() - phone.last;
-      if (timeDistance < 2000) {
-        var a = map( timeDistance, 0, 2000, 255, 0 );
-        fill(phone.color);
-        shader(fx);
-        noStroke();
-        directionalLight(255, 255, 255, -1, -1, -1);
-        translate(phone.x * width, phone.y * height);
-        rotateX(frameCount * 0.01);
-        box(50);
-      }
-      else {
-        phone.x = undefined;
-        phone.y = undefined;
-      }
+    var p = particles[id];
+    p.age *= 0.9;
+    var c = tex.get(
+      p.x * tex.width,
+      p.y * tex.height);
 
-      pop();
-    }
+    var a = (c[0] + c[1] + c[2]) * 0.01;
+    p.x += 0.01 * Math.cos(a);
+    p.y += 0.01 * Math.sin(a);
+
+    pg.push();
+    pg.fill(p.color);
+    pg.shader(fx);
+    pg.translate(p.x * width, p.y * height);
+    pg.rotateX(frameCount * 0.01 + 5 * p.age);
+    pg.rotateY(frameCount * 0.03 - 5 * p.age);
+    var s = 100 * p.age;
+    pg.box(s * 2, s /5, s /5, 1, 1);
+    pg.pop();
+
   }
-  pop();
+  pg.pop();
+
+  particles = particles.filter(function(v, i, a) { return v.age > 0.01; });
+
+  texture(pg);
+  plane(width, height);
 }
